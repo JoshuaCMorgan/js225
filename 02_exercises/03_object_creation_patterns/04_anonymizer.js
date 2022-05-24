6b8e2e8a409470d6e7a4b4172525cf0ec92bb12d
Joshua Morgan <jmorg0605@gmail.com>
2022-05-24 14:43:12 +0800

    completed exercise

--- 02_exercises/03_object_creation_patterns/04_anonymizer.js
+++ 02_exercises/03_object_creation_patterns/04_anonymizer.js
@@ -1,27 +1,113 @@
-let Account = {
-  reanonymize(string) {
-    //code
-  },
+let Account = (function() {
+  function getUserAccount(id, testPassword) {
+    if (users[id].userPassword === testPassword) {
+      return users[id];
+    };
+    return false;
+  }
 
-  resetPassword() {},
+  function getRandomLetterNumber() {
+    let randomIndex = Math.floor(Math.random() * 62);
+    return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ1234567890'[randomIndex];
+  }
 
-  firstName() {},
+  function anonymize() {
+    let result = '';
 
-  lastName() {},
+    for (let i = 0; i < 16; i += 1) {
+      result += getRandomLetterNumber();
+    }
 
-  email() {},
+    return result;
+  }
 
-  displayName() {},
+  let users = {};
 
-  init(email, password, firstName, lastName) {
-    this.email = email;
-    this.password = password;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.displayName = "abcdefghijklmnop"
-  },
-};
+  let generateId = (function() {
+    let userId = 0;
+    return function() {
+      userId += 1;
+      return userId;
+    };
+  })();
+
+  return {
+    init: function(email, password, firstName, lastName) {
+      this.displayName = anonymize();
+      this.id = generateId();
+
+      users[this.id] = {
+        userEmail: email,
+        userPassword: password,
+        userFirstName: firstName,
+        userLastName: lastName,
+      }
+      return this;
+    },
+
+    reanonymize: function(password) {
+      let userAccount = getUserAccount(this.id, password);
+      if (userAccount) {
+        this.displayName = anonymize();
+        return true
+      } else {
+        return 'Invalid Password';
+      }
+    },
+
+    resetPassword: function(password, newPassword) {
+      let userAccount = getUserAccount(this.id, password);
+      if (userAccount) {
+        userAccount.userPassword = newPassword;
+        return true;
+      } else {
+        return 'Invalid Password';
+      }
+    },
+
+    firstName: function(password) {
+      let userAccount = getUserAccount(this.id, password);
+      if (userAccount) {
+        return userAccount.userFirstName;
+      } else {
+        return 'Invalid Password';
+      }
+    },
+
+    lastName: function(password) {
+      let userAccount = getUserAccount(this.id, password);
+      if (userAccount) {
+        return userAccount.userLastName;
+      } else {
+        return 'Invalid Password';
+      }
+    },
+
+    email: function(password) {
+      let userAccount = getUserAccount(this.id, password);
+      if (userAccount) {
+        return userAccount.userEmail;
+      } else {
+        return 'Invalid Password';
+      }
+    },
+  };
+})();
+
 
 let fooBar = Object.create(Account).init('foo@bar.com', '123456', 'foo', 'bar');
+console.log(fooBar.firstName);                     // returns the firstName function
+console.log(fooBar.email);                         // returns the email function
+console.log(fooBar.firstName('123456'));           // logs 'foo'
+console.log(fooBar.firstName('abc'));              // logs 'Invalid Password'
+console.log(fooBar.displayName);                   // logs 16 character sequence
+console.log(fooBar.resetPassword('123', 'abc'))    // logs 'Invalid Password'
+console.log(fooBar.resetPassword('123456', 'abc')) // logs true
 
-console.dir(fooBar);
+let displayName = fooBar.displayName;
+fooBar.reanonymize('abc');                         // returns true
+console.log(displayName === fooBar.displayName);   // logs false
+
+let bazQux = Object.create(Account).init('baz@qux.com', '123456', 'baz', 'qux');
+console.log(fooBar.firstName('abc'));              // logs 'foo'
+console.log(fooBar.email('abc'));                  // logs 'foo@bar.com'
